96d76db1defde633cd137af94bb7cd38a5d68892
Ayaan Fareed <ayaan.fareed2001@gmail.com>
2026-01-08 16:23:45 +0500

    add commit
    updated code

--- src/components/perfume_for_him/for_him.tsx
+++ src/components/perfume_for_him/for_him.tsx
@@ -17,68 +17,71 @@ const For_him = () => {
     const [isMobile, setIsMobile] = useState(false)
     const [ForHim, setForHim] = useState<IForHimType[]>([])
 
+    const data = [
+        {
+            name: 'DREAM INPIRED BY JANNAN',
+            orignal_price: 2000,
+            dicounted_price: 1899.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/Dreame.jpg?v=1760523421&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Dream.jpg?v=1760523421&width=360'
+        },
+
+        {
+            name: 'FORMAL INPIRED BY OFFICE FOR MEN',
+            orignal_price: 2000.00,
+            dicounted_price: 1599.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/Formel.jpg?v=1760522782&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Formal.jpg?v=1760522782&width=360',
+        },
+
+        {
+            name: 'FIERY TOBACCO - INSPIRED BY MANCERA',
+            orignal_price: 2000.00,
+            dicounted_price: 1699.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/fiery_tobacco.jpg?v=1760523091&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Fiery-Tobaco.jpg?v=1760523091&width=360'
+        },
+        {
+            name: 'DARK RAVEN INPIRED BY BLACK OPIUM',
+            orignal_price: 2000.00,
+            dicounted_price: 1899.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/dark_reven.jpg?v=1760522570&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Dark-Raven.jpg?v=1760522570&width=360'
+        },
+        {
+            name: 'ROYAL OUD',
+            orignal_price: 2000.00,
+            dicounted_price: 1799.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/Royal_Oud_32df109e-c80d-4239-8c63-9f9a5a3ac58b.jpg?v=1760522995&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Royal-Oud.jpg?v=1760522995&width=540'
+        },
+
+        {
+            name: 'SALSA SPIRIT - INSPIRED BY CREED AVENTUS',
+            orignal_price: 2000.00,
+            dicounted_price: 1899.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/Salsa_Spirit_40a4eff5-1c16-4bf3-97ad-bcec31a39ded.jpg?v=1760523136&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Salsa-Spirit.jpg?v=1760523136&width=720'
+        },
+        {
+            name: 'ZARAK INSPIRED BY AZZARO WANTED',
+            orignal_price: 2000.00,
+            dicounted_price: 1899.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/zarak.jpg?v=1760522773&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Zarak.jpg?v=1760522773&width=720'
+        },
+        {
+            name: 'VELINA INSPIRED BY BURBURY WEEKEND',
+            orignal_price: 2000.00,
+            dicounted_price: 1699.00,
+            img_src1: 'https://elyscents.pk/cdn/shop/files/Peak_Breeze_ebff756b-2f75-4d81-bb82-fb443abad2ea.jpg?v=1760523055&width=1080',
+            img_src2: 'https://elyscents.pk/cdn/shop/files/ELyscents-Peak-Breeze.jpg?v=1760523055&width=360'
+        },
+    ]
+
     useEffect(() => {
-        setForHim([
-            {
-                name: 'DREAM INPIRED BY JANNAN',
-                orignal_price: 2000,
-                dicounted_price: 1899.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/Dreame.jpg?v=1760523421&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Dream.jpg?v=1760523421&width=360'
-            },
-
-            {
-                name: 'FORMAL INPIRED BY OFFICE FOR MEN',
-                orignal_price: 2000.00,
-                dicounted_price: 1599.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/Formel.jpg?v=1760522782&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Formal.jpg?v=1760522782&width=360',
-            },
-
-            {
-                name: 'FIERY TOBACCO - INSPIRED BY MANCERA',
-                orignal_price: 2000.00,
-                dicounted_price: 1699.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/fiery_tobacco.jpg?v=1760523091&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Fiery-Tobaco.jpg?v=1760523091&width=360'
-            },
-            {
-                name: 'DARK RAVEN INPIRED BY BLACK OPIUM',
-                orignal_price: 2000.00,
-                dicounted_price: 1899.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/dark_reven.jpg?v=1760522570&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Dark-Raven.jpg?v=1760522570&width=360'
-            },
-            {
-                name: 'ROYAL OUD',
-                orignal_price: 2000.00,
-                dicounted_price: 1799.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/Royal_Oud_32df109e-c80d-4239-8c63-9f9a5a3ac58b.jpg?v=1760522995&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Royal-Oud.jpg?v=1760522995&width=540'
-            },
-
-            {
-                name: 'SALSA SPIRIT - INSPIRED BY CREED AVENTUS',
-                orignal_price: 2000.00,
-                dicounted_price: 1899.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/Salsa_Spirit_40a4eff5-1c16-4bf3-97ad-bcec31a39ded.jpg?v=1760523136&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Salsa-Spirit.jpg?v=1760523136&width=720'
-            },
-            {
-                name: 'ZARAK INSPIRED BY AZZARO WANTED',
-                orignal_price: 2000.00,
-                dicounted_price: 1899.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/zarak.jpg?v=1760522773&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/Elyscents-Zarak.jpg?v=1760522773&width=720'
-            },
-            {
-                name: 'VELINA INSPIRED BY BURBURY WEEKEND',
-                orignal_price: 2000.00,
-                dicounted_price: 1699.00,
-                img_src1: 'https://elyscents.pk/cdn/shop/files/Peak_Breeze_ebff756b-2f75-4d81-bb82-fb443abad2ea.jpg?v=1760523055&width=1080',
-                img_src2: 'https://elyscents.pk/cdn/shop/files/ELyscents-Peak-Breeze.jpg?v=1760523055&width=360'
-            },
-        ])
+        const filterProducts = data.filter (item => item.dicounted_price < 1700);
+        setForHim(filterProducts)
     }, [])
 
     useEffect(() => {
