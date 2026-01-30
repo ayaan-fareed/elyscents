9efda7400fba166c95d541c9e837571a0817987b
Ayaan Fareed <ayaan.fareed2001@gmail.com>
2026-01-30 18:42:32 +0500

    add .
    code updated

--- src/components/crazyDeal_comp/crazyDeal.tsx
+++ src/components/crazyDeal_comp/crazyDeal.tsx
@@ -8,6 +8,7 @@ import 'swiper/css'
 
 const data = [
     {
+        id: 1,
         name: '4X PERFUME BUNDLE',
         orignal_price: 7596.00,
         dicounted_price: 5599.00,
@@ -15,6 +16,7 @@ const data = [
         img_src2: 'https://elyscents.pk/cdn/shop/files/4_perfume_new_1.jpg?v=1764960763&width=360'
     },
     {
+        id: 2,
         name: '3X PERFUME BUNDLE',
         orignal_price: 5700.00,
         dicounted_price: 4444.00,
@@ -22,6 +24,7 @@ const data = [
         img_src2: 'https://elyscents.pk/cdn/shop/files/3perfume.jpg?v=1760181185&width=360',
     },
     {
+        id: 3,
         name: 'THE PERFECT DUO',
         orignal_price: 3700.00,
         dicounted_price: 2999.00,
@@ -29,6 +32,7 @@ const data = [
         img_src2: 'https://elyscents.pk/cdn/shop/files/perfect_Duo_2.0.jpg?v=1760428407&width=720'
     },
     {
+        id: 4,
         name: '6X MEGA DEAL',
         orignal_price: 11400.00,
         dicounted_price: 8399.00,
@@ -36,6 +40,7 @@ const data = [
         img_src2: 'https://elyscents.pk/cdn/shop/files/SKU.jpg?v=1764765674&width=540'
     },
     {
+        id: 5,
         name: 'OUD DUO COMBO',
         orignal_price: 3900.00,
         dicounted_price: 3199.00,
@@ -43,6 +48,7 @@ const data = [
         img_src2: 'https://elyscents.pk/cdn/shop/files/ouddeal_1.jpg?v=1766596989&width=360'
     },
     {
+        id: 6,
         name: 'TOP 3 BEST SELLERS',
         orignal_price: 5600.00,
         dicounted_price: 4400.00,
@@ -50,6 +56,7 @@ const data = [
         img_src2: 'https://elyscents.pk/cdn/shop/files/image_5.png?v=1763039447&width=360'
     },
     {
+        id: 7,
         name: 'OFFICE DEAL',
         orignal_price: 5700.00,
         dicounted_price: 4400.00,
@@ -57,6 +64,7 @@ const data = [
         img_src2: 'https://elyscents.pk/cdn/shop/files/officedeal.jpg?v=1765439449&width=360'
     },
     {
+        id: 8,
         name: 'TESTER BOX',
         orignal_price: 1250.00,
         dicounted_price: 990.00,
@@ -66,6 +74,7 @@ const data = [
 ]
 
 type ICrazyProductsType = {
+    id: number,
     name: string,
     orignal_price: number,
     dicounted_price: number,
@@ -88,14 +97,23 @@ const CrazyDeal = () => {
     const CartItem = (value: ICrazyProductsType, index: number) => (
         <div className={style.carts_prnt} key={index}>
             <div className={style.cart_image}>
-                <img
-                    src={
-                        !isMobile && hoverIndex === index
-                            ? value.img_src2
-                            : value.img_src1
-                    }
-                    onMouseEnter={() => !isMobile && setHoverIndex(index)}
-                    onMouseLeave={() => !isMobile && setHoverIndex(null)} alt={value.name} />
+                <Link
+                    href={{
+                        pathname: "/productDetails",
+                        query: { id: value.id },
+                    }}
+                >
+                    <img
+                        src={
+                            !isMobile && hoverIndex === index
+                                ? value.img_src2
+                                : value.img_src1
+                        }
+                        onMouseEnter={() => !isMobile && setHoverIndex(index)}
+                        onMouseLeave={() => !isMobile && setHoverIndex(null)}
+                        alt={value.name}
+                    />
+                </Link>
                 <div className={style.sale_tag}>
                     <p>Sale</p>
                 </div>
@@ -176,7 +194,7 @@ const CrazyDeal = () => {
                         </div>
                     </div>
                 </div>
-                
+
                 {!isMobile ? (
                     <div className={style.crazydeal_parnt}>
                         {CrazyProducts.map((value, index) =>
